2e1e8f6aac39ba535d19a2a488a5814245b16aa7
dragoonzx <dragoonz@yandex.ru>
2017-11-10 12:06:02 +0300

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,20 @@
+name = 'Max'
+print('Hello,' + name + ' would you like to learn some Python today?')
+
+name='max scherbak'
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+name='albert enstein'
+print(name.title() + ' once said, "A person who never made a \n mistake never tried anything new."')
+
+famous_person='albert enstein'
+quote='"A person who never made a \n mistake never tried anything new"'
+print(famous_person.title() +' once said,'+ quote)
+
+user_name=' max '
+print(user_name)
+print(user_name.strip())
+
+#it`s my first programs. today is 09.11.2017
